777ed6b5e901d014a4d344e002d9b9fd44bc23dc
slopjong <git@slopjong.de>
2014-10-09 11:58:37 +0200

    Some uncommitted code

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+define('ROOT_DIR',      __DIR__);
+define('IMAGES_DIR',    ROOT_DIR . '/images');
+define('BUILD_DIR',     ROOT_DIR . '/.tmp');
+define('AUTH_FILE',     BUILD_DIR . '/auth.json');
+define('PROJECT_DIR',   BUILD_DIR . '/project');
+define('ASSETS_DIR',    BUILD_DIR . '/assets');
+define('CACHE_DIR',     BUILD_DIR . '/cache');
+define('DOCKER_FILE',   BUILD_DIR . '/DOCKERFILE');
+define('COMPOSER_PHAR', ROOT_DIR . '/vendor/composer.phar');
+
+if (! file_exists(COMPOSER_PHAR)) {
+    echo 'Download composer' . PHP_EOL;
+//    system('wget -o '. COMPOSER_PHAR .' http://getcomposer.org/composer.phar');
+    system('cd .tmp && wget http://getcomposer.org/composer.phar; cd -');
+}
+
 include 'vendor/DockerFileInterface.php';
 include 'vendor/WebProjectInterface.php';
 include 'vendor/BaseImageInterface.php';
@@ -7,29 +23,26 @@ include 'vendor/BaseImage.php';
 include 'vendor/DebianImageInterface.php';
 include 'vendor/DebianImage.php';
 
-init();
+ob_start();
+include 'vendor/composer.phar';
+ob_end_flush();
+//echo \Composer\Composer::VERSION;
 
-$path = 'images';
+exit();
 
-$objects = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($path),
-    RecursiveIteratorIterator::SELF_FIRST
-);
-
-$images = new Images();
-
-foreach($objects as $fileName => $object){
-    if  (basename($fileName) === 'build.php') {
-        $imageName = str_replace(
-            array('images/', '/build.php'),
-            array('', ''),
-            $fileName
-        );
-
-        $images->add($imageName, $fileName);
-    }
+if (! file_exists(CACHE_DIR)) {
+    mkdir(CACHE_DIR, 0755, true);
 }
 
+if (! file_exists(ASSETS_DIR)) {
+    mkdir(ASSETS_DIR, 0755, true);
+}
+
+if (! file_exists(PROJECT_DIR)) {
+    mkdir(PROJECT_DIR, 0755, true);
+}
+
+$images = new Images();
 $images->buildAll();
 
 /********************************************************************/
@@ -63,6 +76,7 @@ class Images
 
     public function add($imageName, $buildFile)
     {
+        echo $imageName .' loaded'. PHP_EOL;
         $this->buildFiles[$imageName] = include $buildFile;
     }
 
@@ -71,11 +85,10 @@ class Images
      */
     protected function copyAddAssets(BaseImage $image, $imageName)
     {
-        // we are in .tmp when the method is called
-
+        echo 'Copy assets ...'. PHP_EOL;
         $assets = $image->getAssets();
         foreach ($assets as $asset) {
-            system("cp -r ../images/$imageName/$asset assets/");
+            system('cp -r '. IMAGES_DIR .'/'. $imageName .'/'. $asset .' '. ASSETS_DIR);
         }
     }
 
@@ -84,7 +97,6 @@ class Images
      */
     protected function checkoutProjects()
     {
-        $projectRoot = 'project';
         $projects = array();
 
         /** @var WebProjectInterface $build */
@@ -94,35 +106,24 @@ class Images
 
         $projects = array_unique($projects);
 
-        $oldDir = array(getcwd());
-
-        if (! file_exists($projectRoot)) {
-            mkdir($projectRoot);
-        }
-
-        chdir($projectRoot);
-
         // checkout the project or pull the latest commits if the project
         // has already been cloned
         foreach ($projects as $project) {
             $projectName = str_replace('.git', '', array_pop(explode('/', $project)));
-            if (file_exists($projectName)) {
-                $oldDir[] = getcwd();
-                chdir($projectName);
-                system('git pull '. $project);
-                chdir(array_pop($oldDir));
+            if (file_exists(PROJECT_DIR .'/'. $projectName)) {
+                echo "Updating project ...";
+                // @todo submodules may also have been updated
+                system('cd '. PROJECT_DIR .'/'. $projectName .' && git pull && cd -');
             } else {
-                system('git clone '. $project);
+                echo "Cloning project ...";
+                system('git clone --recursive '. $project .' '. PROJECT_DIR .'/'. $projectName);
             }
         }
-
-        chdir(array_pop($oldDir));
     }
 
     public function buildAll()
     {
-        $oldDir = getcwd();
-        chdir('.tmp');
+        $this->loadBuildFiles();
 
         $this->checkoutProjects();
 
@@ -130,42 +131,57 @@ class Images
         foreach ($this->buildFiles as $imageName => $build) {
 
             if (in_array($imageName, $this->posignore)) {
-
-                echo PHP_EOL . PHP_EOL .str_pad("", 80, "#") . PHP_EOL;
-                echo "# SKIPPING $imageName" . PHP_EOL;
-                echo str_pad("", 80, "#") . PHP_EOL . PHP_EOL;
+                $this->outputHeader('SKIPPING'. $imageName);
                 continue;
             }
 
-            echo PHP_EOL . PHP_EOL .str_pad("", 80, "#") . PHP_EOL;
-            echo "# BUILDING $imageName" . PHP_EOL;
-            echo str_pad("", 80, "#") . PHP_EOL . PHP_EOL;
-
+            $this->outputHeader('BUILDING'. $imageName);
             $this->copyAddAssets($build, $imageName);
 
-            $tarFileName = str_replace('/', '_', $imageName);
-
-            // auto-generate the Dockerfile
-            file_put_contents('Dockerfile', $build->toDockerFile());
-
+            file_put_contents(DOCKER_FILE, $build->toDockerFile());
             system("docker build --no-cache=true -t $imageName .");
+
+            $tarFileName = str_replace('/', '_', $imageName);
 //            system("docker save -o $tarFileName.tar $imageName");
 //            system("mv $tarFileName.tar ../builds");
         }
 
         $this->cleanup();
-        chdir($oldDir);
+    }
+
+    protected function loadBuildFiles()
+    {
+        $objects = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(IMAGES_DIR),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach($objects as $fileName => $object){
+            if  (basename($fileName) === 'build.php') {
+                $imageName = str_replace(
+                    array(IMAGES_DIR .'/', '/build.php'),
+                    array('', ''),
+                    $fileName
+                );
+
+                if (! in_array($imageName, $this->posignore)) {
+                    $this->add($imageName, $fileName);
+                }
+            }
+        }
     }
 
     protected function cleanup()
     {
-        unlink('Dockerfile');
-        system('rm -rf assets/*');
+        echo "Cleaning up ..." . PHP_EOL;
+        unlink(DOCKER_FILE);
+        system('rm -rf '. ASSETS_DIR .'/*');
     }
-}
 
-function init() {
-    mkdir('.tmp/cache', 0755, true);
-    mkdir('.tmp/assets', 0755, true);
-    mkdir('.tmp/project', 0755, true);
+    protected function outputHeader($headerMessage)
+    {
+        echo PHP_EOL . PHP_EOL .str_pad("", 80, "#") . PHP_EOL;
+        echo "# $headerMessage" . PHP_EOL;
+        echo str_pad("", 80, "#") . PHP_EOL . PHP_EOL;
+    }
 }
